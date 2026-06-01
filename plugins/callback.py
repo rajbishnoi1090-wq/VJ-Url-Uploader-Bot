407e8462698d6b638174c6679cdebc375caf4935
rajbishnoi1090-wq <rajbishnoi.1090@gmail.com>
2026-06-01 14:03:50 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -36,11 +36,12 @@ async def button(bot, update):
             reply_markup=Translation.TECH_VJ_HELP_BUTTONS,
             # disable_web_page_preview=True
         )
-   elif update.data == "about":
-    await update.answer("About button clicked!", show_alert=True)
-            reply_markup=Translation.TECH_VJ_ABOUT_BUTTONS,
-            # disable_web_page_preview=True
-        )
+    elif update.data == "about" 
+        await update.message.edit(
+    
+text=Translation.TECH_VJ_ABOUT_TEXT,
+    reply_markup=Translation.TECH_VJ_ABOUT_BUTTONS,
+)
     elif "close" in update.data:
         await update.message.delete(True)
 
